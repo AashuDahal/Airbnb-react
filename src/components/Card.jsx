0f1611d434f1f064b58564578d83f354efcca55f
AashutoshDahal22 <aashutoshdahal22@gmail.com>
2023-09-11 14:34:52 +0545

    props inclued
    image doesnot render

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import image1 from '../assets/cardimage.png'
-import starimg from '../assets/starimage.png'
+// import image1 from '../assets/cardimage.png'
+// import starimg from '../assets/starimage.png'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className='card_div'>
-        <img src={image1} className='card_image' />
+        <img src='../assets/cardimage.png' className='card_image' />
         <div className='desc'>
-        <img src={starimg} className="card--star" />
-                <span>5.9</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+        <img src={`./assets/${props.starimg}`} className="card--star" />
+                <span>{props.rating}</span>
+                <span className="gray">{props.rat}</span>
+                <span className="gray">{props.country}</span>
         </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>   
+            <p>{props.text}</p>
+            <p><span className="bold">{props.price}</span></p>   
     </div>
   )
 }
-
+//{`../assets/${props.image}`}
 export default Card
